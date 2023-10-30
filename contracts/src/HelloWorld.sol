cb0030896a56851048c9091e3cfcd90a5c30b207
defispartan <andrew@aave.com>
2023-10-29 22:52:18 -0500

    feat: emit actor in HelloWorld

--- contracts/src/HelloWorld.sol
+++ contracts/src/HelloWorld.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 import {IHelloWorld} from "./interfaces/IHelloWorld.sol";
 
 contract HelloWorld is IHelloWorld {
-    event Greet(string message);
+    event Greet(string message, address actor);
 
     function helloWorld(string memory message) public {
-        emit Greet(string(abi.encodePacked("Hello, World! ", message)));
+        emit Greet(string(abi.encodePacked("Hello, World! ", message)), msg.sender);
     }
 }
